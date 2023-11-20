17d116b16a79b25e96f3f00e25ce83c5bbc2ce97
melanimb <92109474+melanimb@users.noreply.github.com>
2023-11-20 13:10:30 +0100

    feat: animate navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import Motion from '../Motion'
 import './navbar.scss'
 
 function Navbar () {
@@ -5,13 +6,22 @@ function Navbar () {
     <div className='navbar'>
       {/* Sidebar */}
       <div className="wrapper">
-        <span>Melani Dev</span>
-        <div className="social">
-          <a href="#"><img src="/facebook.png" alt="" /></a>
-          <a href="#"><img src="/instagram.png" alt="" /></a>
-          <a href="#"><img src="/youtube.png" alt="" /></a>
-          <a href="#"><img src="/dribbble.png" alt="" /></a>
-        </div>
+        <Motion>
+          <span>Melani Mares</span>
+        </Motion>
+        <Motion>
+          <div className="social">
+            <a href="#" target='_blank' rel='noreferrer'>
+              <img src="/facebook.png" alt="facebook icon" />
+            </a>
+            <a href="https://www.instagram.com/melani.mares/" target='_blank' rel='noreferrer'>
+              <img src="/instagram.png" alt="instagram icon" />
+            </a>
+            <a href="https://www.linkedin.com/in/melania-mares-321034262/" target='_blank' rel='noreferrer'>
+              <img src="/linkedin.png" alt="linkedin icon" />
+            </a>
+          </div>
+        </Motion>
       </div>
     </div>
   )
