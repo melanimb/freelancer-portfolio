e6c45f987df7f52d0ebcefb6b527ca9b484089fc
melanimb <92109474+melanimb@users.noreply.github.com>
2023-11-25 20:36:42 +0100

    feat: add Parallax

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './app.scss'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/hero/Hero'
+import Parallax from './components/parallax/Parallax'
+import Services from './components/services/Services'
 
 const App = () => {
   return <div>
@@ -8,9 +10,15 @@ const App = () => {
       <Navbar />
       <Hero />
     </section>
-    <section id='Servicios'>Parallax</section>
-    <section>Services</section>
-    <section id='Trabajos'>Parallax</section>
+    <section id='Servicios'>
+      <Parallax type='services' />
+    </section>
+    <section>
+      <Services />
+    </section>
+    <section id='Trabajos'>
+      <Parallax type='works' />
+    </section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
