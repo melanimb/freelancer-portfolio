6611b3206ffd602bfb12a7e5ea4481853192c87d
melanimb <92109474+melanimb@users.noreply.github.com>
2023-11-30 12:42:31 +0100

    feat: change facebook per github

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -12,14 +12,14 @@ function Navbar () {
         </Motion>
         <Motion>
           <div className="social">
-            <a href="#" target='_blank' rel='noreferrer'>
-              <img src="/facebook.png" alt="facebook icon" />
-            </a>
-            <a href="https://www.instagram.com/melani.mares/" target='_blank' rel='noreferrer'>
-              <img src="/instagram.png" alt="instagram icon" />
+            <a href="https://github.com/melanimb" target='_blank' rel='noreferrer'>
+              <img src="/github.png" alt="github icon" />
             </a>
             <a href="https://www.linkedin.com/in/melania-mares-321034262/" target='_blank' rel='noreferrer'>
               <img src="/linkedin.png" alt="linkedin icon" />
+            </a>
+            <a href="https://www.instagram.com/melani.mares/" target='_blank' rel='noreferrer'>
+              <img src="/instagram.png" alt="instagram icon" />
             </a>
           </div>
         </Motion>
