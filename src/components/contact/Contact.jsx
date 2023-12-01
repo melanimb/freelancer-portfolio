44f9cb68bfb4c3d64668b73d13b39fc3e36c4063
melanimb <92109474+melanimb@users.noreply.github.com>
2023-12-01 11:23:36 +0100

    fix: scroll contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,11 +5,10 @@ import emailjs from '@emailjs/browser'
 
 const variants = {
   initial: {
-    y: 500,
+    y: 0,
     opacity: 0
   },
   animate: {
-    y: 0,
     opacity: 1,
     transition: {
       duration: 0.5,
